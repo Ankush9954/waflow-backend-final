e6c063c69efe0f7d37e2d461b74a9ebd53a7aca4
Ankush9954 <ankush9954@gmail.com>
2025-09-24 12:50:33 +0530

    Adding Auth Service

---
+++ services/user-service/src/models/agentModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const agentSchema = new mongoose.Schema({
+  agentId: { type: String, unique: true }, // e.g. AG-0001
+  tenantId: { type: String, required: true },
+  fullName: { type: String, required: true },
+  email: { type: String, required: true },
+  phoneNumber: { type: String },
+  status: { type: String, enum: ["active", "inactive"], default: "active" },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Manager" },
+  createdAt: { type: Date, default: Date.now },
+  role: { type: String, default: "agent" },
+});
+
+export default mongoose.model("Agent", agentSchema);
